f2844ad2797089063814ac14be5defaa37288039
Jawad Azizi <jawad.azizi1398@gmail.com>
2023-12-02 23:58:14 +0430

    chapter sevet: fetching data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { Card } from "../ui/dashboard/cards";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 import NavLinks from "../ui/dashboard/nav-links";
+import RevenueChart from "../ui/dashboard/revenue-chart";
 
 export default function Page(){
-    return <p>Dashboard Page</p>
+    return (
+    <>
+    <p>Dashboard Page</p>
+    <Card />
+    <RevenueChart />
+    <LatestInvoices />
+
+    </>)
 }
